411009d645e986069b046467b6f3f5c76055f80d
Nitish@123 <nitish6206774025@gmail.com>
2025-11-30 21:06:07 +0530

    vercel change

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,12 @@
 export const BACKEND_URL = "https://nitish-prajapati-portfolio.onrender.com";
 // export const BACKEND_URL = "http://localhost:5000";
+
+
+// {
+//     "rewrites": [
+//         {
+//         "source": "/(.*)",
+//             "destination": "/"
+//         }
+//     ]
+// }
